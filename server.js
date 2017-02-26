32fdb00c5570e40953859ef8788926ba55a9d517
Muhammad Fauzi <fauzi87sg@gmail.com>
2017-02-27 03:39:08 +0800

    added delete route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var Hapi = require('hapi')
+var uuid = require('uuid')
 
 var server = new Hapi.Server()
+
+var cards = {}
 
 server.connection({port: 3000})
 
@@ -30,27 +33,52 @@ server.route({
 
 server.route({
   path: '/cards/new',
-  method: 'GET',
-  handler: function (request, reply) {
-    reply.file('/templates/new.html')
-  }
-})
-
-server.route({
-  path: '/cards/new',
-  method: 'POST',
-  handler: function (request, reply) {
-    reply.redirect('/cards')
-  }
+  method: ['GET', 'POST'],
+  handler: newCardHandler
 })
 
 server.route({
   path: '/cards',
   method: 'GET',
-  handler: function (request, reply) {
-    reply.file('templates/cards.html')
-  }
+  handler: cardsHandler
 })
+
+server.route({
+  path: '/cards/{id}',
+  method: 'DELETE',
+  handler: deleteCardHandler
+})
+
+function newCardHandler (request, reply) {
+  if (request.method === 'get') {
+    reply.file('/templates/new.html')
+  } else {
+    var card = {
+      name: request.payload.name,
+      recipient_email: request.payload.recipient_email,
+      sender_name: request.payload.sender_name,
+      sender_email: request.payload.sender_email,
+      card_image: request.payload.card_image
+    }
+    saveCard(card)
+    console.log(cards)
+    reply.redirect('/cards')
+  }
+}
+
+function cardsHandler (request, reply) {
+  reply.file('templates/cards.html')
+}
+
+function deleteCardHandler (request, reply) {
+  delete cards[request.params.id]
+}
+
+function saveCard (card) {
+  var id = uuid.v1()
+  card.id = id
+  cards[id] = card
+}
 
 server.start(function () {
   console.log('Listening on ' + server.info.uri)
