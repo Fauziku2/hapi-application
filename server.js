9031553048fc3a7d587c6356d79c3714e63146fb
Muhammad Fauzi <fauzi87sg@gmail.com>
2017-03-05 11:46:37 +0800

    restructuring the app

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var Hapi = require('hapi')
-var uuid = require('uuid')
-var fs = require('fs')
-var Joi = require('joi')
-var Boom = require('boom')
-
+var CardStore = require('./lib/cardStore')
 var server = new Hapi.Server()
+
+CardStore.initialize()
 
 var cards = loadCards()
 
@@ -62,95 +60,7 @@ server.ext('onPreResponse', function (request, reply) {
   reply.continue()
 })
 
-server.route({
-  path: '/',
-  method: 'GET',
-  handler: function (request, reply) {
-    reply.file('/templates/index.html')
-  }
-})
-
-server.route({
-  path: '/assets/{path*}',
-  method: 'GET',
-  handler: {
-    directory: {
-      path: './public',
-      listing: false
-    }
-  }
-})
-
-server.route({
-  path: '/cards/new',
-  method: ['GET', 'POST'],
-  handler: newCardHandler
-})
-
-server.route({
-  path: '/cards',
-  method: 'GET',
-  handler: cardsHandler
-})
-
-server.route({
-  path: '/cards/{id}',
-  method: 'DELETE',
-  handler: deleteCardHandler
-})
-
-var cardSchema = Joi.object().keys({
-  name: Joi.string().min(3).max(50).required(),
-  recipient_email: Joi.string().required(),
-  sender_name: Joi.string().min(3).max(50).required(),
-  sender_email: Joi.string().email().required(),
-  card_image: Joi.string().regex(/.+\.(jpg|bmp|png|gif)\b/).required()
-})
-
-function newCardHandler (request, reply) {
-  if (request.method === 'get') {
-    reply.view('new', {card_image: mapImages()})
-  } else {
-    Joi.validate(request.payload, cardSchema, function (err, val) {
-      if (err) {
-        return reply(Boom.badRequest(err.details[0].message))
-      }
-      var card = {
-        name: request.payload.name,
-        recipient_email: val.recipient_email,
-        sender_name: val.sender_name,
-        sender_email: val.sender_email,
-        card_image: val.card_image
-      }
-      saveCard(card)
-      reply.redirect('/cards')
-    })
-  }
-}
-
-function cardsHandler (request, reply) {
-  reply.view('cards', {cards: cards})
-}
-
-function deleteCardHandler (request, reply) {
-  delete cards[request.params.id]
-  reply()
-}
-
-function saveCard (card) {
-  var id = uuid.v1()
-  card.id = id
-  cards[id] = card
-}
-
-function loadCards () {
-  var file = fs.readFileSync('./cards.json')
-  return JSON.parse(file.toString())
-}
-
-function mapImages () {
-  return fs.readdirSync('./public/images/cards')
-}
+server.route(require('./lib/routes'))
 
 server.start(function () {
   console.log('Listening on ' + server.info.uri)
